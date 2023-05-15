9a48122cd47e15a4f15f683a32837c5f182ee8d8
To_Quy_Thanh <Toquythanh2k3@gmail.com>
2023-05-15 18:48:03 +0700

    feat: calculate and display the sum of odd natural numbers from 1 to 100

--- Practice_session_1/Lesson4.py
+++ Practice_session_1/Lesson4.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""Lesson 4: Program to calculate and display the sum of odd natural numbers from 1 to 100."""
+
+count = int(0)
+for i in range(0, 101):
+	if i % 2 == 0:
+		count+=i
+print(f"sum of odd natural numbers from 1 to 100 is {count}")
