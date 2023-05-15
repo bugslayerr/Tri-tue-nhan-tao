f0fb6600dee728e2c84ac33fd21ef16dd1b1a9cd
To_Quy_Thanh <Toquythanh2k3@gmail.com>
2023-05-15 17:45:40 +0700

    refactor: Exception handling a = 0 is not a quadratic equation

--- Practice_session_1/Lesson3.py
+++ Practice_session_1/Lesson3.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 """Program to solve first-order equations ax + b = 0, where a, b are the real coefficients entered from the keyboard."""
-
-a = int(input("Enter a: "))
-b = int(input("Enter b: "))
-
-print(f"x = -b/a => x = {-b/a}")
+try:
+	a = int(input("Enter a: "))
+	b = int(input("Enter b: "))
+	result = -b/a
+	print(f"x = -b/a => x = {-b/a}")
+except ZeroDivisionError:
+	print("number a have to differ 0")
