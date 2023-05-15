64c3bfacb9689e824468bb17738f176d9f577d09
Tô Quý Thành <Toquythanh2k3@gmail.com>
2023-05-15 15:41:56 +0700

    feat: calculates and displays the area and perimeter of that rectangle

---
+++ Practice_session_1/Lesson1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+width = int(input("Enter width of rectange: "))
+length = int(input("Enter length of rectange: "))
+
+print(f"The area of the rectangle is {width * length}")
+print(f"The perimeter of the rectangle is {(width + length) * 2}")
