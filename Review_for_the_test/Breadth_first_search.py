ae9fe39ce756713ad413fd707a8bb5d48ceccc3c
To_Quy_Thanh <Toquythanh2k3@gmail.com>
2023-05-15 23:06:21 +0700

    feat: definition of read_file_csv function

--- Review_for_the_test/Breadth_first_search.py
+++ Review_for_the_test/Breadth_first_search.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+import pandas as pd
+import numpy as np
+
+def read_file_csv(path):
+	data = pd.read_csv(path, index_col=0)
+	data_array = np.array(data)
+	return data_array
+
+def main():
+	path = r"data_Breath_first_search.csv"
+	data_array = read_file_csv(path)
+	print(data_array)
+
+if __name__ == '__main__':
+	main()
